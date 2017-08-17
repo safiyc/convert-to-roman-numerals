9f971b61a62224951b424e0ec6c958bb19521634
Erik Zakrzewski and Safiy Cham <erik@>
2017-08-17 12:03:53 -0700

    add working for while loop

---
+++ README.md
@@ -0,0 +1,14 @@
+SPECS
+
+* Ignore non-numeric characters
+* Return "I" for input of "1"
+* Return "II" for input of "2"
+* Return "III" for input of "3"
+* Return "V" for input of "5"
+* Return "IV" for input of "4"
+* Return "X" for input of "10"
+* Return "IX" for input of "9"
+* Return "L" for input of "50"
+* Return "C" for input of "100"
+* Return "D" for input of "500"
+* Return "M" for input "1000"
---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+
+var converter = function(num) {
+
+var result = "";
+var numberInput = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+var newNumber = "";
+var romans = ["M", "CM", "D", 'CD', "C", "XC", "L", "XL", "X", "IX",  "V", "IV", "I" ];
+
+
+for (var i = 0; i < numberInput.length; i++) {
+    while (numberInput[i] <= num) {
+      result += romans[i];
+      num -= numberInput[i];
+    }
+
+}
+return result;
+
+debugger
+}
+
+
+$(document).ready(function() {
+  $("form#regularNumber").submit(function(event) {
+    event.preventDefault();
+  var numberInput = parseInt($("input#number").val());
+    var result = converter(numberInput);
+    $(".well").show();
+    $(".rNumeral").text(result);
+
+  });
+});
+
+
+// function one(numberInput) {
+//   if (numberInput === 1){
+//     return romans[12];
+//   }
+//   else if (numberInput === 5) {
+//        return romans[1];
+//   }
+//   else if (numberInput === 10) {
+//        return romans[2];
+//   }
+//   else if (numberInput === 50) {
+//        return romans[3];
+//   }
+//   else if (numberInput === 100) {
+//        return romans[4];
+//   }
+//   else if (numberInput === 500) {
+//        return romans[5];
+//   }
+//   else if (numberInput === 1000) {
+//        return romans[6];
+//   }
+// };
