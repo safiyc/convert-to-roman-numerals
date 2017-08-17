b7733327be2a6d7d3700de7fcd00cef39f7094f3
Erik Zakrzewski and Safiy Cham <erik@>
2017-08-17 13:22:46 -0700

    change js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var numberInput = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 var newNumber = "";
 var romans = ["M", "CM", "D", 'CD', "C", "XC", "L", "XL", "X", "IX",  "V", "IV", "I" ];
 
-
+debugger
 for (var i = 0; i < numberInput.length; i++) {
     while (numberInput[i] <= num) {
       result += romans[i];
